76c9498c64481ced45e2cc4957e1c4c76cbffdea
hp <hp@LAPTOP-LOTEGNE8>
2022-08-26 16:09:13 +0530

    Second commit

---
+++ BATCH_M24/src/com/cg/core/ProgramOne.java
@@ -0,0 +1,13 @@
+package com.cg.core;
+
+public class ProgramOne {
+	public static void main(String[]args)
+	{
+int num1,num2,sum;
+num1=95;
+num2=45;
+sum=num1+num2;
+System.out.println("Addition ="+sum);
+	
+	}
+}
