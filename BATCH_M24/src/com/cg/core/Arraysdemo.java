bacdecb244129492f13eecf3ae6e7eeb1ca926c5
hp <hp@LAPTOP-LOTEGNE8>
2022-10-08 12:50:25 +0530

    First commit

--- BATCH_M24/src/com/cg/core/Arraysdemo.java
+++ BATCH_M24/src/com/cg/core/Arraysdemo.java
@@ -3,7 +3,8 @@ package com.cg.core;
 import java.util.Arrays;
 
 public class Arraysdemo {
-
+	// length--->Array and length()--->string
+	
 	public static void main(String[] args) {
 		int[] arr= {10,58,75,68,12,96,38,74,18,96};
 		System.out.println("Entered Array is=");
@@ -20,6 +21,8 @@ public class Arraysdemo {
 			System.out.println(num+" ");
 			
 		}
+		System.out.println("length of the Array is=");
+		System.out.println(arr.length);
 	}
 
 }
